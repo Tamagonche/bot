bad6410acb40726e61db6ca09ff3bd706847757e
Pepe <173433735+pepe-onche@users.noreply.github.com>
2025-03-24 03:05:39 +0100

    add notify_dead and notify_hunger

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os
+import asyncio
+import random
+from typing import Any
+
 import onchebot
 from dotenv import load_dotenv
 from onchebot.models import Message
-from supabase import create_client, Client
+from supabase import create_async_client, AsyncClient
 
 load_dotenv()
 
@@ -15,7 +19,7 @@ onchebot.setup(
     prometheus_port=int(os.environ.get("PROMETHEUS_PORT", 9464)),
 )
 
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase: AsyncClient | None = None
 
 user = onchebot.add_user(
     username=os.environ.get("ONCHE_USERNAME", ""),
@@ -27,22 +31,66 @@ tamagonche = onchebot.add_bot("tamagonche", user, TOPIC_ID)
 @tamagonche.command("nourrir")
 async def feed(msg: Message, _):
     try:
-        supabase.table("actions").insert({"type": "feed", "username": msg.username, "pet_id": 1}).execute()
+        await supabase.table("actions").insert({"type": "feed", "username": msg.username, "pet_id": 1}).execute()
     except:
         pass
 
 @tamagonche.command("nettoyer")
 async def clean_trash(msg: Message, _):
     try:
-        supabase.table("actions").insert({"type": "clean_trash", "username": msg.username, "pet_id": 1}).execute()
+        await supabase.table("actions").insert({"type": "clean_trash", "username": msg.username, "pet_id": 1}).execute()
     except:
         pass
 
 @tamagonche.command("doliprane")
 async def give_medicine(msg: Message, _):
     try:
-        supabase.table("actions").insert({"type": "give_medicine", "username": msg.username, "pet_id": 1}).execute()
+        await supabase.table("actions").insert({"type": "give_medicine", "username": msg.username, "pet_id": 1}).execute()
     except:
         pass
 
-onchebot.start()
+async def notify_dead():
+    await tamagonche.post_message("Je suis MORT :rip:")
+
+async def notify_hunger():
+    text = random.choice([
+        "J'ai faim !",
+        "Je dois manger",
+        "J'ai le ventre vide",
+        "QUI va me donner à manger, BORDEL",
+        "DE LA BOUFFE PUTAIN",
+        "Je veux un burgourent",
+        "Quelqu'un veut bien me donner un kebab ?",
+        "Il me faut des fritent",
+    ])
+    await tamagonche.post_message(f"{text} :tamagonche:")
+
+prev_pet: dict[str, Any] | None = None
+
+async def on_pet_update(payload):
+    pet = payload["data"]["record"]
+
+    if prev_pet and prev_pet["food"] != pet["food"]:
+        if pet["food"] == 0:
+            await notify_dead()
+        elif pet["food"] <= pet["hunger_threshold"]:
+            await notify_hunger()
+
+async def init():
+    global supabase
+    supabase = await create_async_client(SUPABASE_URL, SUPABASE_KEY)
+
+    def callback(payload):
+        asyncio.create_task(on_pet_update(payload))
+
+    asyncio.create_task(
+        supabase.channel("prod")
+        .on_postgres_changes("UPDATE", schema="public", table="pets", callback=callback)
+        .subscribe()
+    )
+
+async def close():
+    if supabase:
+        await supabase.realtime.close()
+
+onchebot.start(init, close)
